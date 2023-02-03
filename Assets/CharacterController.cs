f0ff4d9b10fbe0914405d38cf690920294d52326
JimmyPlit <plitjimmyaaron@gmail.com>
2023-02-03 23:17:42 +0200

    Scores
    Implemented score

--- Assets/CharacterController.cs
+++ Assets/CharacterController.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:1e3392ef10dc77778975bc1e99e561f035e37fd2b1a7e6ddab4dd3cad04dc244
-size 1555
+oid sha256:7888e8503d9c124f6bbfecd2678f2eed377321221404ad464735cec4641e2d90
+size 1705
