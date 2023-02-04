37cc2f1cb99c3e95dc2cfebbcbf7545a9475df6d
JimmyPlit <plitjimmyaaron@gmail.com>
2023-02-04 11:11:43 +0200

    Main is back

--- Assets/Scripts/UiController.cs
+++ Assets/Scripts/UiController.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:7c139d41e8d1aba1bc39ce854b53ec2a6e3ec3b9e6480c2545ba02a75f452045
-size 553
+oid sha256:ccf3efc85858f66716a92789edad71d25899e33e51e46953015a20898b55c557
+size 555
