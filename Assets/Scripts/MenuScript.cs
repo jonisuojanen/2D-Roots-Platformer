45bea6437d1acad6019054c897929023e9c7c473
Deserui <bjosu005@edu.xamk.fi>
2023-02-04 22:58:52 +0200

    Title art

--- Assets/Scripts/MenuScript.cs
+++ Assets/Scripts/MenuScript.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:2039163162d2e37640a49a62ef18230802116e2eb7fe4b8dc0575ed1133e2e57
-size 406
+oid sha256:dc2f99eb2c123239d16023e566d660eb64ed33a42cca91b2b77d5563964f544d
+size 412
