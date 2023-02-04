52f69aae87fc4ef30e48d816cf10107a1e371f04
JimmyPlit <plitjimmyaaron@gmail.com>
2023-02-04 18:08:26 +0200

    EnemyAi

--- Assets/Scripts/MiddleEnemyAI.cs
+++ Assets/Scripts/MiddleEnemyAI.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:735636c2a1ba6ce2e7affacc5a22189aa30dd78255b4b9fe40963b72f6391a82
-size 449
+oid sha256:78e03fff0b367326068af9ba281a6c955f11666f33de5d7214f2ca7e59597bff
+size 599
