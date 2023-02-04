2a4c31610665c92ea26569467d75472ed3e3d978
JimmyPlit <plitjimmyaaron@gmail.com>
2023-02-04 16:05:42 +0200

    DeathBarrier

--- Assets/Scripts/MiddleEnemy.cs
+++ Assets/Scripts/MiddleEnemy.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:396a8fc057c9d5a8d634753abf6088ae229e536895aca1a497833a0504a527f0
-size 517
+oid sha256:417105a0950fad63a2ffd5351e7b18eb24402381379dfea7e9ec49731acc42ec
+size 665
--- Assets/Scripts/bird.cs
+++ Assets/Scripts/bird.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:31dfc241dbc815be1dd97d88339117ebcb3103ea487efda21e5bb02f30c07698
-size 517
+oid sha256:47159cb8d0f57bb853fc509e19fbeadb6b4b3d2138791b41ca04cfc0f47bb7cf
+size 665
