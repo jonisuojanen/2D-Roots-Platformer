c5b5ca8aea110c92ed69ec9de6a6f89a4b678a03
Deserui <bjosu005@edu.xamk.fi>
2023-02-05 11:11:45 +0200

    Delete useless crap

--- Root game/Library/PackageCache/com.unity.2d.animation@7.0.7/Editor/SkinningModule/CopyTool.cs
+++
@@ -1,3 +0,0 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:af005ebcde11e094c8c5388af4ddf484b07d8f217152265e6045ffd848ad1ee1
-size 25946
--- Root game/Library/PackageCache/com.unity.2d.animation@7.0.7/Editor/SkinningModule/SkinningModule.cs
+++
@@ -1,3 +0,0 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:8f2d4fe712243f13e563893b58ad080632bfe692451c470053deca166a5c7232
-size 24701
